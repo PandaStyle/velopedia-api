be0d0320bd7e3985e7439ac7f680cd706cecdd11
Zsolt Nemeth <Zsolt.Nemeth@logmein.com>
2016-03-31 09:24:34 -0400

    correct feedUrl order

--- modules/feedUrlLookup.js
+++ modules/feedUrlLookup.js
@@ -1,11 +1,12 @@
 module.exports = {
+    "bikeradar" : "http://www.bikeradar.com",
+    "cyclingindustry" : "http://www.cyclingindustry.news",
     "cyclingnews" : "http://www.cyclingnews.com/news",
     "cyclingtips" : "http://cyclingtips.com",
+    "cyclingweekly" : "http://www.cyclingweekly.co.uk",
+    "pezcycling" : "http://www.pezcyclingnews.com",
     "roadcc" : "http://road.cc/rss",
     "roadbikeaction" : "http://roadbikeaction.com",
-    "cyclingweekly" : "http://www.cyclingweekly.co.uk",
     "velonews" : "http://velonews.competitor.com",
-    "bikeradar" : "http://www.bikeradar.com",
-    "pezcycling" : "http://www.pezcyclingnews.com",
     "theinnerring": "http://inrng.com"
 };
