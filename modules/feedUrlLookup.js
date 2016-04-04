53f060c9b5c6ec56e58c946f6f3b56dce7e726ed
Zsolt Nemeth <Zsolt.Nemeth@logmein.com>
2016-04-04 15:45:54 -0400

    add pelotonmagazine

--- modules/feedUrlLookup.js
+++ modules/feedUrlLookup.js
@@ -5,6 +5,7 @@ module.exports = {
     "cyclingtips" : "http://cyclingtips.com",
     "cyclingweekly" : "http://www.cyclingweekly.co.uk",
     "pezcycling" : "http://www.pezcyclingnews.com",
+    "pelotonmagazine" : "http://www.pelotonmagazine.com",
     "roadcc" : "http://road.cc/rss",
     "roadbikeaction" : "http://roadbikeaction.com",
     "sbs": "http://www.sbs.com.au/cyclingcentral/rss/articles",
